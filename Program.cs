46785217fec5968aeadff6ba88ac7d7f8cfceeb7
Justin27tw <158735080+Justin27tw@users.noreply.github.com>
2024-06-06 09:49:43 +0800

    買鉛筆
    Problem D
    買鉛筆
    鉛筆一支5 元,一打50元。小明需要幫班上每位同學買一枝鉛筆,請問要多少錢?由於小明
    很注重環保,他絕不會為了省錢而多買任何不需要的東西。也就是說,小明買的鉛筆數量一
    定等於班上的人數。
    Input Format
    輸入只有一列,為小明班上的人數n,1 ≤ n ≤ 200。
    Output Format
    請輸出一個數字,代表這次採購的金額。
    Sample Input 1
    42
    Sample Input 2
    11
    Hint
    Sample Output 1
    180
    Sample Output 2
    55

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pen_domjudg
+{
+    class CCount
+    {
+        int pen=12;
+        int index12;
+        int index5;
+        int money;
+        public void Counting(int pencount)
+        {
+            index12= pencount / pen;
+            index5= pencount % pen;
+            
+            money += (index12 * 50)+(index5*5);
+            Console.Write(money);
+          
+        }
+       
+    }
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        { CCount count1 = new CCount();
+            Console.Write("鉛筆幾隻?");
+            int pen_count=Convert.ToInt32(Console.ReadLine());
+            if (pen_count >= 1 && pen_count <= 200)
+            { count1.Counting(pen_count );
+                
+            }
+
+            Console.Read();
+        }
+    }
+}
