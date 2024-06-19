7070fd7af39c0df717783a3adc353a1bff855b9d
Justin27tw <158735080+Justin27tw@users.noreply.github.com>
2024-06-19 19:23:51 +0800

    抽象類別vs介面

--- Program.cs
+++ Program.cs
@@ -3,56 +3,86 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-namespace Temp//命名空間 Temp 生存範圍第6行~第30行
-{
-    class CCircle
+
+namespace 抽象類別
+{ abstract class CShape//抽象類別
     {
-        const double pi = 3.14;   //const固定值不能更改，圓周率
-        double radius;  //半徑
-        public CCircle(double r)//用建構元給予半徑值
-        {
-            if (r > 0)
-            {
-                radius = r;
-            }
-            else
-            {
-                radius = 1.0;
-            }
 
+        protected String color;//存取修飾詞=>保護
+        public void setColor(String str)//方法
+        { 
+         color = str;
         }
-        public double Area()//圓面積計算
+        public abstract void show();//抽象方法，只做好模板未實作，記得最後加上;
+    }
+    class CRectangle : CShape//類別繼承抽象類別
+    { 
+        protected int width,height;
+        public CRectangle(int w, int h)//方法
         {
-            return radius * radius * pi;
+            width = w;
+            height = h;
         }
+        public override void show()//改寫，並實作
+        {
+            Console.Write("color=" + color + ", ");
+            Console.WriteLine("area=" + width * height);
+        }
+    }
+    class CCircle : CShape
+    {
+        protected double radius;
+        public CCircle(double r) 
+        { 
+        radius = r;
+        }
+        public override void show()
+        {
+            Console.Write("color=" + color + ", ");
+            Console.WriteLine("area="+3.14*radius*radius);
+        }
+    }
 
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CRectangle rect = new CRectangle(5, 10);
+            rect.setColor("Yellow");
+            rect.show();
+            CCircle cir = new CCircle(2.0);
+            cir.setColor("Green");
+            cir.show();
+            Console.Read();
+        }
     }
 }
-    namespace _0606classroom//命名空間 _0606classroom 生存範圍第31行~第58行
+//抽象類別abstract VS 介面interface
+/*抽象類別裡面可以有一般的方法，也可以有抽象方法，只要有抽象方法就要在class的時候加上abstract
+//定義時方法如同只給大方向未說清楚內容，繼承下去後自己改
+abstract class 類別名
+{
+    public abstract 資料型態 方法名();
+}
+class 類別名:父類別
+{
+    public (new/override) 資料型態 方法名() //此處為改寫並實作
     {
-    using Temp; //引入其他命名空間
-
-        internal class Program
-        {
-            static void Main(string[] args)
-            {
-                //如果想在 0606classroom命名空間 使用其他命名空間的類別或方法或建構元
-                /*有兩種做法 
-                       1.在現在的命名空間中使用的其他命名空間之類別還有方法和建構元
-                前面加上 原本所屬的命名空間之名稱ex: 在Temp 裡面有class CCircle類別 
-                如果想在0606classroom這個命名空間使用CCircle就要變成 Temp.CCircle
-                就這像檔案管理員中，C:\作業\Temp\CCircle 檔案所在的路徑 
-                       2.using = >告訴系統說我要使用這個命名空間，
-                系統才知道要引入該命名空間，所以系統才能辨識
-                ex：第33行 using Temp 這樣底下不用再 Temp.CCircle*/
-
-                CCircle cir1 = new CCircle(9.5);//產生物件並直接用建構元呼叫
-                CCircle cir2 = new CCircle(8.0);
-                Console.Write("面積=" + cir2.Area());
-
-            Console.Read();
-
-            }
-        }
+    ...................//實作
     }
+}
 
+---------------------------------------------------------------------------------------------
+//定義規格，在子類別實作內容
+interface 類別名  //裡面的方法不能有public private abstract 因為已經預設好了不能多寫
+{
+    資料型態 方法名();//一樣為抽象方法未實作
+}
+class 類別名:介面名
+{
+   資料型態 方法名()
+    {
+...............//實作
+    }
+}
+ */
